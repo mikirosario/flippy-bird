b03ac664581b4630540123cc7d730e9a7c4520af
mikiencolor <mrosariokling@outlook.com>
2024-07-14 18:08:16 +0200

    Added jumping feature to the player script.

--- Player.gd
+++ Player.gd
@@ -2,7 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
+const GRAVITY = 300.0
+const JUMP = -150.0
 
-func _physics_process(delta):
-	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * SPEED
+func _process(delta):
+	velocity.y += delta * GRAVITY
+	var jumpMultiplier = min(max(inverse_lerp(-250.0, 15.0, position.y), 0.0), 1.0)
+	var velMultiplier = 1.0 - min(max(inverse_lerp(0, JUMP, velocity.y), 0.0), 1.0)
+	if Input.is_action_just_pressed("ui_select"):
+		velocity.y += JUMP * jumpMultiplier * velMultiplier
+	print(jumpMultiplier)
+	velocity.x = SPEED
 	move_and_slide()
