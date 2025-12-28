ce978c55b2a4ad33a52305a41541c7481b4414d5
Miki Rosario <chisparroja@gmail.com>
2025-12-28 02:44:37 +0100

    Balloon spawning.

--- balloon.gd
+++ balloon.gd
@@ -5,21 +5,18 @@ const MINHEIGHT = 165
 const GRAVITY = 300.0
 
 @onready var AnimatedSprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var Bouyancy: float = randf_range(25, 100)
 var velocity: Vector2 = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 
 func _physics_process(delta: float) -> void:
 	var highAltFactor = clamp(inverse_lerp(MINHEIGHT, MAXHEIGHT, position.y), 0.0, 1.0) # Increases to 1.0 as height approaches MAXHEIGHT
 	var lowAltFactor = 1.0 - highAltFactor # Increases to 1.0 as height approaches MINHEIGHT
-	var lift = -(GRAVITY + 50)
+	var lift = -(GRAVITY + Bouyancy)
 	if velocity.y > 0:
 		lift += -velocity.y
 	lift *= lowAltFactor
 	velocity.y += (GRAVITY + lift) * delta
-	print("Gravity + Lift: %s + %s" % [GRAVITY, lift])
-	print("HighAltFactor: %s" % highAltFactor)
-	print("LowAltFactor: %s" % lowAltFactor)
-	print("Vel: %s" % velocity.y)
 	velocity.x = SPEED * lowAltFactor
 	# Animator.speed_scale = highAltFactor + 0.2
 	# rotation_degrees
@@ -28,7 +25,6 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	AnimatedSprite.play("Pop")
-	print("Body Entered: %s" % velocity.y)
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	queue_free()
